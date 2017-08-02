4a55b0a4ef795255b8782ffa85521df9184184ca
Haoran Feng <haoran.feng@alo7.com>
2017-08-02 15:54:12 +0800

    format

--- 001-phone-number.py
+++ 001-phone-number.py
@@ -1,10 +1,14 @@
-from igraph import *
-g = Graph.Tree(13, 3)
+import igraph as ig
+g = ig.Graph.Tree(13, 3)
 layout = g.layout_reingold_tilford(mode="in", root=[0])
 g.vs['size'] = ['60']
-g.vs['color'] = ['#d9534f', '#5cb85c', '#5cb85c', '#5cb85c', '#428bca','#428bca','#428bca','#428bca','#428bca','#428bca','#428bca','#428bca','#428bca']
-g.vs['label'] = ['[ ]', '[a]', '[b]', '[c]', '[ad]', '[ae]', '[af]', '[bd]', '[be]', '[bf]', '[cd]', '[ce]', '[cf]']
+g.vs['color'] = ['#d9534f', '#5cb85c', '#5cb85c', '#5cb85c', '#428bca',
+                 '#428bca',
+                 '#428bca', '#428bca', '#428bca', '#428bca', '#428bca',
+                 '#428bca', '#428bca']
+g.vs['label'] = ['[ ]', '[a]', '[b]', '[c]', '[ad]', '[ae]',
+                 '[af]', '[bd]', '[be]', '[bf]', '[cd]', '[ce]', '[cf]']
 g.vs['label_size'] = ['20']
 
 g.write_svg('001.svg', layout=layout, vertex_size=20)
-plot(g, layout=layout,bbox = (800, 300), margin = 50)
+ig.plot(g, layout=layout, bbox=(800, 300), margin=50)
