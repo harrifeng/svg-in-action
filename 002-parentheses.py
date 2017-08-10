2ba0715a77e46c311c19cb26cc9412f535d0ab9f
Haoran Feng <haoran.feng@alo7.com>
2017-08-10 16:02:49 +0800

    ok, before insert content

--- 002-parentheses.py
+++ 002-parentheses.py
@@ -3,15 +3,22 @@ from util import get_color_arr
 N = 31
 g = ig.Graph.Tree(N, 2)
 print g
-g.delete_vertices(range(23, 31))
-g.delete_vertices(range(11, 15))
-g.delete_vertices([2, 5, 6])
 
-layout = g.layout_reingold_tilford(mode="in", root=[0])
+del_nodes = [5, 6]
+
 g.vs['size'] = ['60']
 g.vs['color'] = get_color_arr(N)
 g.vs['label'] = range(31)
 g.vs['label_size'] = ['20']
+
+del_nodes.extend(range(15, 17))
+del_nodes.extend(range(21, 31))
+del_nodes.extend(range(11, 15))
+g.delete_vertices(del_nodes)
+
+
+layout = g.layout_reingold_tilford(mode="in", root=[0])
+
 
 g.write_svg('002.svg', layout=layout, vertex_size=20)
 ig.plot(g, layout=layout, bbox=(800, 300), margin=50)
