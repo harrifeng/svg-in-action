459033c50cd037eac4483894c24560192298a187
Haoran Feng <haoran.feng@alo7.com>
2017-08-10 15:54:23 +0800

    it works now

--- 002-parentheses.py
+++ 002-parentheses.py
@@ -3,7 +3,10 @@ from util import get_color_arr
 N = 31
 g = ig.Graph.Tree(N, 2)
 print g
-# g.delete_vertices(6)
+g.delete_vertices(range(23, 31))
+g.delete_vertices(range(11, 15))
+g.delete_vertices([2, 5, 6])
+
 layout = g.layout_reingold_tilford(mode="in", root=[0])
 g.vs['size'] = ['60']
 g.vs['color'] = get_color_arr(N)
