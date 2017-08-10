db7df95106a09865133aaa3273a6028d89930507
Haoran Feng <haoran.feng@alo7.com>
2017-08-10 15:49:55 +0800

    cool

--- 002-parentheses.py
+++ 002-parentheses.py
@@ -1,17 +1,13 @@
 import igraph as ig
-g = ig.Graph.Tree(31, 2)
+from util import get_color_arr
+N = 31
+g = ig.Graph.Tree(N, 2)
 print g
 # g.delete_vertices(6)
 layout = g.layout_reingold_tilford(mode="in", root=[0])
 g.vs['size'] = ['60']
-
-
-g.vs['color'] = ['#d9534f',
-                 '#5cb85c', '#5cb85c',
-                 '#428bca', '#428bca', '#428bca', '#428bca'
-                 ]
-
-g.vs['label'] = ['0'] * 31
+g.vs['color'] = get_color_arr(N)
+g.vs['label'] = range(31)
 g.vs['label_size'] = ['20']
 
 g.write_svg('002.svg', layout=layout, vertex_size=20)
